d7781a375c4071cec103edf0f7e9902c65dddbd6
Venkat Akhil Lakkapragada <lvakhil06@gmail.com>
2026-04-26 13:36:44 +0530

    Remove TrainerAgent from agents module
    Removed TrainerAgent from imports and __all__ list.

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,7 +1,6 @@
 from agents.monitor import MonitorAgent
 from agents.responder import ResponderAgent
 from agents.breaker import BreakerAgent
-from agents.trainer import TrainerAgent
 
 __all__ = [
     "MonitorAgent",
